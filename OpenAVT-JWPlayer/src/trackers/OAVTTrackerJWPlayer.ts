e82beb9a7ba970f0618612439a7798bf91c8c05a
Andreu Santaren Llop <andreu.santaren@gmail.com>
2021-08-07 20:50:58 +0200

    Init JWPlayer tracker project.

---
+++ OpenAVT-JWPlayer/src/trackers/OAVTTrackerJWPlayer.ts
@@ -0,0 +1,19 @@
+import { OAVTEvent, OAVTInstrument, OAVTLog, OAVTState, OAVTTrackerInterface } from 'openavt-core-js'
+
+export class OAVTTrackerJWPlayer implements OAVTTrackerInterface {
+    initEvent(event: OAVTEvent): OAVTEvent {
+        OAVTLog.debug("JWPlayer Tracker received event = ", event)
+        return event
+    }
+    
+    trackerId: number;
+    state: OAVTState = new OAVTState();
+
+    instrumentReady(instrument: OAVTInstrument): void {
+        OAVTLog.debug("JWPlayer Tracker instrumentReady")
+    }
+
+    endOfService(): void {
+        OAVTLog.debug("JWPlayer Tracker endOfService")
+    }
+}
---
+++ OpenAVT-JWPlayer/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = (env, argv) => {
+  return {
+    entry: {
+      index: path.resolve(__dirname, "./dist/esm/index.js")
+    },
+    output: {
+      path: path.resolve(__dirname, "./dist/umd"), // builds to ./dist/umd/
+      filename: "openavt-jwplayer.js", // index.js
+      library: "OpenAVT", // aka window.myLibrary
+      libraryTarget: "umd", // supports commonjs, amd and web browsers
+      globalObject: "this"
+    },
+    module: {
+      rules: [{ test: /\.t|js$/, use: "babel-loader" }]
+    }
+  };
+};
